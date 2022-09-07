a2fe5885b90f29e419aab4e929f952c7ac973102
korapatdev <korapatsittkool@gmail.com>
2022-09-07 14:48:38 +0700

    update

--- pages/checkgps/[jelId]/[name]/[desciption]/[lat]/[long]/[length]/[startFile]/[resultFile]/[eventId]/[cpId].js
+++ pages/checkgps/[jelId]/[name]/[desciption]/[lat]/[long]/[length]/[startFile]/[resultFile]/[eventId]/[cpId].js
@@ -6,6 +6,7 @@ import { FaMapMarkedAlt } from "react-icons/fa";
 import Link from "next/link";
 import { getDistance } from "geolib";
 import Swal from "sweetalert2";
+import Loding from "../../../../../../../../../../../components/Loding";
 const CpIdT = () => {
   const router = useRouter();
   const {
@@ -85,55 +86,59 @@ const CpIdT = () => {
     calDistance();
   };
   // console.log(myLocation.lat);
-  return (
-    <div>
-      <div className="flex flex-col justify-center items-center h-screen p-4">
-        <h5 className="w-full  bg-black  tracking-tight text-white p-4 rounded-t-3xl dark:text-white">
-          Check point {cpId}
-        </h5>
-        <a className="block p-8 w-full bg-white rounded-b-2xl border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 className="font-bold text-2xl mt-2 text-center text-[#f12757]  dark:text-gray-400">
-            {name}
+  if (myLocation.lat > 0) {
+    return (
+      <div>
+        <div className="flex flex-col justify-center items-center h-screen p-4">
+          <h5 className="w-full  bg-black  tracking-tight text-white p-4 rounded-t-3xl dark:text-white">
+            Check point {cpId}
           </h5>
-          <p className=" bg-white p-2 h-52  text-center text-back shadow-inner shadow-slate-500/50   mt-4 pt-4 py-2 px-4 rounded">
-            {desciption}
-          </p>
+          <a className="block p-8 w-full bg-white rounded-b-2xl border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="font-bold text-2xl mt-2 text-center text-[#f12757]  dark:text-gray-400">
+              {name}
+            </h5>
+            <p className=" bg-white p-2 h-52  text-center text-back shadow-inner shadow-slate-500/50   mt-4 pt-4 py-2 px-4 rounded">
+              {desciption}
+            </p>
 
-          <button
-            className="bg-gradient-to-r from-[#f78830]  to-[#fc252f] w-full mt-4  text-white font-bold py-2 px-4  rounded-full"
-            onClick={onCheckLocation}
-          >
-            Next
-          </button>
-        </a>
-        <div className="flex justify-start font-bold  text-white mt-10 items-center gap-4">
-          <Link href={`/checkpoint/${eventId}`}>
-            <div className=" flex flex-col justify-center items-center">
-              <div className=" text-2xl">
-                <AiFillHome />
-              </div>
-              <div>HOME</div>
-            </div>
-          </Link>
-          <div>
-            <a
-               href={`https://www.google.com/maps/dir/${myLocation.lat},${myLocation.lng}/${lat},${long}/data=!3m1!4b1!4m2!4m1!3e2`}
-              //  href={`https://www.google.com/maps/dir/${lat},${long}/${myLocation.lat},${myLocation.lng}/data=!3m1!4b1!4m2!4m1!3e2`}
-              target="_blank"
-              rel="noreferrer"
+            <button
+              className="bg-gradient-to-r from-[#f78830]  to-[#fc252f] w-full mt-4  text-white font-bold py-2 px-4  rounded-full"
+              onClick={onCheckLocation}
             >
+              Next
+            </button>
+          </a>
+          <div className="flex justify-start font-bold  text-white mt-10 items-center gap-4">
+            <Link href={`/checkpoint/${eventId}`}>
               <div className=" flex flex-col justify-center items-center">
                 <div className=" text-2xl">
-                  <FaMapMarkedAlt />
+                  <AiFillHome />
                 </div>
-                <div> MAP</div>
+                <div>HOME</div>
               </div>
-            </a>
+            </Link>
+            <div>
+              <a
+                href={`https://www.google.com/maps/dir/${myLocation.lat},${myLocation.lng}/${lat},${long}/data=!3m1!4b1!4m2!4m1!3e2`}
+                //  href={`https://www.google.com/maps/dir/${lat},${long}/${myLocation.lat},${myLocation.lng}/data=!3m1!4b1!4m2!4m1!3e2`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className=" flex flex-col justify-center items-center">
+                  <div className=" text-2xl">
+                    <FaMapMarkedAlt />
+                  </div>
+                  <div> MAP</div>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }else{
+    return <Loding />;
+  }
 };
 
 export default CpIdT;
