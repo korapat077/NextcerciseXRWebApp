0cb92da6e5bcf2afc96d70689aea3b5c7950fd3b
korapatdev <korapatsittkool@gmail.com>
2022-09-07 10:46:59 +0700

    edit function

--- components/Unpublish.js
+++ components/Unpublish.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function () {
+export default  function Unpublish () {
   return (
     <div className=" flex justify-center items-center h-screen ">
     <div className="text-center">
       <div role="status">
-        <h5 className=" font-bold mt-5  text-white">Event hasn't started yet</h5>
+        <h5 className=" font-bold mt-5  text-white">Event hasn started yet</h5>
         <span className="sr-only">Event has been canceled</span>
       </div>
     </div>
